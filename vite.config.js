1d83c333f8f5a10e77a67d13ed558684124f1fcb
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-10-27 17:43:08 -0500

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://frankhs899.github.io/github-hashrouter',
+  base: 'https://frankhs899.github.io/github-hashrouter/',
 });
